f70798e6e9fc10bb2851da787b089b1186890309
blckvia <a.vyatkinn@icloud.com>
2024-02-03 12:30:36 +0300

    feat: add logic to Getfile handler

--- internal/grpc/gateway/server.go
+++ internal/grpc/gateway/server.go
@@ -4,18 +4,39 @@ import (
 	"context"
 	distributedStoragev1 "distributed-file-storage/protos/gen/go/distributedStorage"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+type Gateway interface {
+	GetFile(ctx context.Context, filename string) ([]byte, error)
+	Upload(stream distributedStoragev1.DistributedStorage_UploadServer) error
+	mustEmbedUnimplementedDistributedStorageServer()
+}
 
 type ServerAPI struct {
 	distributedStoragev1.UnimplementedDistributedStorageServer
+	gateway Gateway
 }
 
-func Register(gRPC *grpc.Server) {
-	distributedStoragev1.RegisterDistributedStorageServer(gRPC, &ServerAPI{})
+func Register(gRPC *grpc.Server, gateway Gateway) {
+	distributedStoragev1.RegisterDistributedStorageServer(gRPC, &ServerAPI{gateway: gateway})
 }
 
 func (s *ServerAPI) GetFile(ctx context.Context, req *distributedStoragev1.GetfileRequest) (*distributedStoragev1.GetfileResponse, error) {
-	panic("Not implemented")
+	if req.GetFilename() == "" {
+		return nil, status.Error(codes.InvalidArgument, "filename is empty")
+	}
+
+	// TODO: implement file getter via gateway service
+	data, err := s.gateway.GetFile(ctx, req.GetFilename())
+	if err != nil {
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &distributedStoragev1.GetfileResponse{
+		Data: data,
+	}, nil
 }
 
 func (s *ServerAPI) Upload(stream distributedStoragev1.DistributedStorage_UploadServer) error {
