c476d230dac7721a575678bff5544b05b5d79be0
blckvia <a.vyatkinn@icloud.com>
2024-02-05 12:03:58 +0300

    feat: add migrator

---
+++ cmd/migrator/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/golang-migrate/migrate/v4"
+)
+
+func main() {
+	var storagePath, migrationsPath, migrationsTable string
+
+	flag.StringVar(&storagePath, "storage-path", "", "path to storage")
+	flag.StringVar(&migrationsPath, "migrations-path", "", "path to migrations")
+	flag.StringVar(&migrationsTable, "migrations-table", "migrations", "name of migrations table")
+	flag.Parse()
+
+	if storagePath == "" {
+		panic("storage-path is required")
+	}
+
+	if migrationsPath == "" {
+		panic("migrations-path is required")
+	}
+
+	m, err := migrate.New(
+		"file://"+migrationsPath,
+		// TODO: adapt to postgresql
+		fmt.Sprintf("posgresql://%s?x-migrations-table=%s", storagePath, migrationsTable))
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := m.Up(); err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			fmt.Println("no migrations to apply")
+
+			return
+		}
+
+		panic(err)
+	}
+
+	fmt.Println("migrations applied successfully")
+}
