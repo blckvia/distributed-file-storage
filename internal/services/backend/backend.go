6e515b37107a36815f988d0f05733f41d5bb56f2
blckvia <a.vyatkinn@icloud.com>
2024-03-27 13:48:50 +0300

    feat: add backend layer

--- internal/services/backend/backend.go
+++ internal/services/backend/backend.go
@@ -1,1 +1,45 @@
 package backend
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+)
+
+type Backend struct {
+	log       *slog.Logger
+	bUploader BlobUploader
+	bGetter   BlobGetter
+}
+
+type BlobUploader interface {
+	Upload(ctx context.Context, blob []byte) error
+}
+
+type BlobGetter interface {
+	Get(ctx context.Context, blobID int64) ([]byte, error)
+}
+
+// New returns a new instance of the Backend service.
+func New(
+	log *slog.Logger,
+	blobUploader BlobUploader,
+	blobGetter BlobGetter,
+) *Backend {
+	return &Backend{
+		log:       log,
+		bUploader: blobUploader,
+		bGetter:   blobGetter,
+	}
+}
+
+// Upload uploads a blob to the backend.
+func (b *Backend) Upload(ctx context.Context, blob []byte, index uint, metaID int64) error {
+	fmt.Println(index, metaID)
+	return b.bUploader.Upload(ctx, blob)
+}
+
+// Get downloads a blob from the backend.
+func (b *Backend) Get(ctx context.Context, blobID int64) ([]byte, error) {
+	return b.bGetter.Get(ctx, blobID)
+}
