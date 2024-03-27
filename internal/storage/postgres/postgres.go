6982517908d79c68501005801a93f3e8a02c1195
blckvia <a.vyatkinn@icloud.com>
2024-03-27 13:49:21 +0300

    update storage

--- internal/storage/postgres/postgres.go
+++ internal/storage/postgres/postgres.go
@@ -1,1 +1,59 @@
 package postgres
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"github.com/lib/pq"
+	// _ "github.com/lib/pq"
+
+	"distributed-file-storage/internal/storage"
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+func New(storagePath string) (*Storage, error) {
+	const op = "storage.postgres.New"
+
+	db, err := sql.Open("postgres", storagePath)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &Storage{
+		db: db,
+	}, nil
+}
+
+func (s *Storage) Stop() error {
+	return s.db.Close()
+}
+
+func (s *Storage) SaveMeta(ctx context.Context, filename, mimeType string) (int64, error) {
+	const op = "storage.postgres.SaveMeta"
+
+	stmt, err := s.db.PrepareContext(ctx, "INSERT INTO meta (filename, mime_type) VALUES ($1, $2)")
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	res, err := stmt.ExecContext(ctx, filename, mimeType)
+	if err != nil {
+		var pqErr *pq.Error
+		if errors.As(err, &pqErr) && pqErr.Code == "unique_violation" {
+			return 0, fmt.Errorf("%s: %w", op, storage.ErrFileAlreadyExists)
+		}
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return id, nil
+}
